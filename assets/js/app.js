73abda3979933b6cd2a6c6a647a53e4426607b2b
unknown <sanjeevmaharjan19@gmail.com>
2021-10-27 18:50:03 +0545

    remaining: scroll to top button, modal gallery for portfolio and navigation highlights

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,3 +45,84 @@ function removeSelection(){
 		opt.classList.remove("text-primary");
 	})
 }
+
+//for lightbox modal gallery of gallery section
+const modal = document.getElementById("lightboxModal");
+const slides = document.querySelectorAll(".slides");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const closeBtn = document.querySelector(".close-button");
+const galleryImages = document.querySelectorAll(".galleryImgs");
+
+galleryImages.forEach((img,index)=>{
+	img.addEventListener("click",function(){
+		modal.style.display = "block";
+		displaySlide(slideIndex = index + 1);
+	})
+})
+
+closeBtn.addEventListener("click", function(){
+	modal.style.display = "none";
+});
+
+nextBtn.addEventListener("click", function(){
+	nextSlide(1)
+})
+
+prevBtn.addEventListener("click", function(){
+	nextSlide(-1)
+})
+
+function nextSlide(n){
+	displaySlide(slideIndex += n)
+}
+
+var slideIndex = 1;
+
+function displaySlide(n){
+
+	if(n > slides.length){
+		slideIndex = 1;
+	}
+	if(n < 1){
+		slideIndex = slides.length;
+	}
+	slides.forEach((slide,index)=>{
+		slide.style.display = "none";
+	});
+
+	slides[slideIndex - 1].style.display = "block";
+
+}
+
+//for hightlighting navigation links when the user scrolls to that particular section//
+//Note: find a better way of doing this//
+
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll",()=>{
+	console.log(window.scrollY);
+	switch(window.scrollY){
+		case 408:
+			navLinks[1].classList.remove("text-dark");
+			navLinks[1].classList.add("text-primary");
+		break;
+		case 2040:
+			navLinks[2].classList.remove("text-dark");
+			navLinks[2].classList.add("text-primary");
+		break;
+		case 2652:
+			navLinks[3].classList.remove("text-dark");
+			navLinks[3].classList.add("text-primary");
+		break;	
+		case 4335:
+			navLinks[4].classList.remove("text-dark");
+			navLinks[4].classList.add("text-primary");
+		break;	
+		case 5559:
+			navLinks[6].classList.remove("text-dark");
+			navLinks[6].classList.add("text-primary");
+		break;			
+	}
+})
